047d2da28d505b171b28aef4118bab372db00daf
IBRAHIM SABER <ibrahimsamosee@gmail.com>
2024-08-01 21:04:01 +0300

    use props to handle the outputs

--- react/day 1/vite-project/src/App.tsx
+++ react/day 1/vite-project/src/App.tsx
@@ -1,18 +1,25 @@
 
 import Product from './components/Product'
 import './App.css'
+import { props } from './components/Product'
+
+const Products:props[] = [
+  {title: "I", desc: "pro 1 desc", price:"200"},
+  {title: "II", desc: "pro 2 desc", price:"250"},
+  {title: "III", desc: "pro 3 desc", price:"300"},
+  {title: "IV", desc: "pro 4 desc", price:"150"}
+]
 
 
 function App() {
 
-
-
   return (
     <main>
-    <Product />
-    <Product />
-    <Product />
-    <Product />
+    <Product title="product1" price="200$" desc="pro1 description"/>
+    <Product title="product2" price="205$" desc="pro2 description"/>
+    <Product title="product3" price="105$" />
+
+
     </main>
   )
 }
--- react/day 1/vite-project/src/components/Product.tsx
+++ react/day 1/vite-project/src/components/Product.tsx
@@ -1,9 +1,16 @@
 
-function Product() {
+export type props={
+    title: string;
+    price: string;
+    desc?: string;
+}
+
+function Product({title,price,desc}: props) {
     return(
         <div>
-            <h1>product 1</h1>
-            <p>product Desc</p>
+            <h1>{title}</h1>
+            <p>{desc}</p>
+            <p>{price}</p>
             <div>
                 <button>Add To Cart</button>
                 <button>WishList</button>
