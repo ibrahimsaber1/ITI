8431a7c57b8e7951756c8bc0431c18259e63932d
IBRAHIM SABER <ibrahimsamosee@gmail.com>
2024-08-03 21:02:49 +0300

    add codezoone third React app files

---
+++ react/00 CodeZone/third-app/src/App.tsx
@@ -0,0 +1,18 @@
+// import { useState } from 'react'
+import './App.css'
+import Counter from './components/Counter';
+import Counterfc from './components/Counterfc';
+
+function App() {
+  // const [count, setCount] = useState(0)
+  
+
+  return (
+    <>
+      <Counter />
+      <Counterfc />
+    </>
+  )
+}
+
+export default App
