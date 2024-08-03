0e47bfdb65e0ea3c26e3c5f1515dd608dbbf6091
IBRAHIM SABER <ibrahimsamosee@gmail.com>
2024-08-03 21:20:21 +0300

    list.jsx : fech the data in the useEffect hock

--- react/00 CodeZone/fourth-app/src/components/List.jsx
+++ react/00 CodeZone/fourth-app/src/components/List.jsx
@@ -1,7 +1,15 @@
+import { useEffect } from "react";
 import Products_data from "../products";
 import Product from "./Product";
 
 function List() {
+  useEffect(()=>{
+    fetch('https://dummyjson.com/products')
+    .then((res) => res.json())
+    .then((data)=>console.log(data));
+  }
+)
+
   const products = Products_data.map((product) => {
     return product.price < 50? <Product product={product} key={product.id}/> : null
     ;
--- react/00 CodeZone/fourth-app/src/components/Product.jsx
+++ react/00 CodeZone/fourth-app/src/components/Product.jsx
@@ -1,5 +1,9 @@
 function Product(props){
     return (
+
+      
+
+
         <div  className="product-cart">
         <img src={props.product.image} alt="Product" width={150}/>
       <h4>{props.product.title}</h4>
