f63341086bc8b2776c9ded8652cf3653042cb12d
IBRAHIM SABER <ibrahimsamosee@gmail.com>
2024-08-03 15:15:10 +0300

    add codezone second React app files

---
+++ react/00 CodeZone/second-app/src/App.tsx
@@ -0,0 +1,18 @@
+// import { useState } from 'react'
+import './App.css'
+import List from './components/List.jsx';
+
+
+function App() {
+
+
+
+  return (
+    <>
+
+      <List />
+    </>
+  )
+}
+  
+export default App
